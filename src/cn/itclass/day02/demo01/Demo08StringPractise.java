439fb31fdbfc1791efb8f19a85b0a206863742b3
hexingyunPC <1106253603@qq.com>
2022-02-12 21:45:34 +0800

    练习题

---
+++ src/cn/itclass/day02/demo01/Demo08StringPractise.java
@@ -0,0 +1,41 @@
+package cn.itclass.day02.demo01;
+/*
+* 定义一个方法，把数组{1,2,3}按照指定格式拼接成一个字符串。格式参照如下：[word1#word2#word3]
+* 思路：
+* 数组数组toString + #
+*
+*
+* int 转换为String
+*  1,空用字符串拼接 ，
+*  2,String.valueof(i),
+*  3,Integer.toString(i);
+*  4,Integer j = i;   j.toSrtring();
+*
+* 方法使用：
+* 接收对象 = 方法名（调用对象）；
+* 方法：
+* public static 返回类型 方法名(接收对象){
+*   return 返回类型
+* }
+*
+* */
+public class Demo08StringPractise {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        String str = fromArrayToString(arr);
+        System.out.println(str);
+    }
+    public static String fromArrayToString(int[] arr) {
+        String str = "[";
+        for (int i = 0; i < arr.length; i++) {
+            str += arr[i];
+            if(i == arr.length - 1){
+                str += "]";
+            }else {
+                str += "#";
+            }
+        }
+
+        return str;
+    }
+}
---
+++ src/cn/itclass/day02/demo01/Demo09StringPractise.java
@@ -0,0 +1,46 @@
+package cn.itclass.day02.demo01;
+
+import java.util.Scanner;
+
+/*
+* 题目：
+* 键盘输入一个字符串，并且统计其中各种字符出现的次数
+* 种类：大写字母，小写字母，数字，其他；
+*
+* 思路：
+* 1，既然要用到键盘输入，肯定是Scanner
+* 2,输入的是字符串， String str = sc.next();
+* 3,需要对每一个字符检查，需要用到toCharArray()
+* 4,遍历char[]字符数组，对当前种类进行判断，并且用四个变量++
+* 5，打印四个变量。分别代表的次数
+*
+* */
+public class Demo09StringPractise {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个字符串");
+        String str = sc.next();
+
+        int countUpper = 0;
+        int countLower = 0;
+        int countNumber = 0;
+        int countOther = 0;
+
+        char[] charArray = str.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            char ch = charArray[i];
+            if('A'<= ch && ch <= 'Z'){
+                countUpper++;
+            }else if('a' <= ch && ch <= 'z'){
+                countLower++;
+            }else if('0' <= ch && ch <= '9'){
+                countNumber++;
+            }else {countOther++;}
+
+        }
+
+        System.out.println("大写字母，小写字母，数字，其他分别代表的次数：");
+        System.out.println(countUpper + "," + countLower + "," + countNumber + "," + countOther);
+
+    }
+}
