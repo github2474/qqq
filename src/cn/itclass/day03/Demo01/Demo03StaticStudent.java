6c90cfbc6d7eaf904d821d93eea830712555ae01
hexingyunPC <1106253603@qq.com>
2022-02-16 21:58:37 +0800

    2月16日

---
+++ src/cn/itclass/day03/Demo01/Demo03StaticStudent.java
@@ -0,0 +1,20 @@
+package cn.itclass.day03.Demo01;
+
+public class Demo03StaticStudent {
+
+    public static void main(String[] args) {
+        //
+        Student.room = "101教室";
+        Student one = new Student("郭炯",20);
+        System.out.println("one的姓名是：" + one.getName());
+        System.out.println("one的姓名是：" + one.getAge());
+        System.out.println("one的姓名是：" + Student.room);
+        System.out.println("===================================");
+
+        Student two = new Student("黄蓉",18);
+        System.out.println("two的姓名是：" + two.getName());
+        System.out.println("two的年龄是：" + two.getAge());
+        System.out.println("two的教室是：" + Student.room);
+    }
+
+}
